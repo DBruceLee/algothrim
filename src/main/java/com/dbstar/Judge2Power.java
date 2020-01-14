3cc0f6f07856b8ddfff5855f5906ca89abe55460
DBruceLee <1204504782>
2020-01-15 00:09:04 +0800

    新增: 1. 判断1个数是否为2的整数次幂

---
+++ src/main/java/com/dbstar/Judge2Power.java
@@ -0,0 +1,30 @@
+package com.dbstar;
+
+import java.util.Date;
+
+/**
+ * 判断一个数是否为2的整数次幂
+ */
+public class Judge2Power {
+    public static void main(String[] args) {
+        System.out.println(new Date().getTime());
+//        System.out.println(new Judge2Power().judge2Power(Integer.MAX_VALUE));
+        System.out.println(new Date().getTime());
+        System.out.println(new Judge2Power().judge2Power_2(Integer.MAX_VALUE));
+        System.out.println(new Date().getTime());
+    }
+
+    public boolean judge2Power(int num) {
+        int temp = 1;
+        while (temp <= num) {
+            if (temp == num) {
+                return true;
+            }
+            temp *= 2;
+        }
+        return false;
+    }
+    public boolean judge2Power_2(int num) {
+        return (num & (num -1)) == 0;
+    }
+}
