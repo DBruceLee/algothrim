c6a0cb3bfa5ff3da07a7cace5f204c79cbf47b09
DBruceLee <1204504782>
2020-01-15 00:13:28 +0800

    新增: 1. 判断1个数是否为2的整数次幂

--- src/main/java/com/dbstar/Judge2Power.java
+++ src/main/java/com/dbstar/Judge2Power.java
@@ -10,8 +10,9 @@ public class Judge2Power {
         System.out.println(new Date().getTime());
 //        System.out.println(new Judge2Power().judge2Power(Integer.MAX_VALUE));
         System.out.println(new Date().getTime());
-        System.out.println(new Judge2Power().judge2Power_2(Integer.MAX_VALUE));
+//        System.out.println(new Judge2Power().judge2Power_2(Integer.MAX_VALUE));
         System.out.println(new Date().getTime());
+        System.out.println(new Judge2Power().judge2Power_3(Integer.MAX_VALUE));
     }
 
     public boolean judge2Power(int num) {
@@ -24,7 +25,19 @@ public class Judge2Power {
         }
         return false;
     }
+
     public boolean judge2Power_2(int num) {
-        return (num & (num -1)) == 0;
+        int temp = 1;
+        while (temp <= num) {
+            if (temp == num) {
+                return true;
+            }
+            temp <<= 1;
+        }
+        return false;
+    }
+
+    public boolean judge2Power_3(int num) {
+        return (num & (num - 1)) == 0;
     }
 }
